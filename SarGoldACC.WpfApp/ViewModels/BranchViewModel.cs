d2622c4841b889036c68e92b251ac0214125bbd7
moosavi <s.ha.mousavi.nazar@gmail.com>
2025-05-07 14:22:27 +0330

    تکمیل ویرایش شعبه

--- SarGoldACC.WpfApp/ViewModels/BranchViewModel.cs
+++ SarGoldACC.WpfApp/ViewModels/BranchViewModel.cs
@@ -1,4 +1,5 @@
-﻿using SarGoldACC.Core.DTOs;
+﻿using System.Collections.ObjectModel;
+using SarGoldACC.Core.DTOs;
 using SarGoldACC.Core.DTOs.Branch;
 using SarGoldACC.Core.Services.Interfaces;
 using SarGoldACC.WpfApp.Helpers;
@@ -18,6 +19,13 @@ public class BranchViewModel : ViewModelBase
         set => SetProperty(ref _branchName, value);
     }
     
+    private ObservableCollection<BranchDto> _allBranches = new();
+    public ObservableCollection<BranchDto> AllBranches
+    {
+        get => _allBranches;
+        set => SetProperty(ref _allBranches, value);
+    }
+    
     public bool CanAccessBranchView => _authorizationService.HasPermission("Branch.View");
     public bool CanAccessBranchCreate => _authorizationService.HasPermission("Branch.Create");
     public bool CanAccessBranchEdit => _authorizationService.HasPermission("Branch.Edit");
@@ -30,6 +38,13 @@ public class BranchViewModel : ViewModelBase
     {
         _authorizationService = authorizationService;
         _branchService = branchService;
+        AllBranches = new ObservableCollection<BranchDto>();
+        
+        // بارگذاری تنظیمات و داده‌ها
+        Task.Run(async () =>
+        {
+            await LoadBranchesAsync();
+        }).GetAwaiter().GetResult();
     }
 
     public async Task SaveBranch()
@@ -40,7 +55,21 @@ public class BranchViewModel : ViewModelBase
         };
         if (_editingBranchId.HasValue)
         {
-            
+            var dto = new BranchDto
+            {
+                Id = _editingBranchId.Value,
+                Name = BranchName
+            };
+            result = await _branchService.UpdateAsync(dto);
+            _editingBranchId = null;
+            if (result.Success)
+            {
+                MessageBoxHelper.ShowSuccess("شعبه با موفقیت ویرایش شد.");
+            }
+            else
+            {
+                MessageBoxHelper.ShowError(result.Message);
+            }
         }
         else
         {
@@ -57,6 +86,29 @@ public class BranchViewModel : ViewModelBase
             {
                 MessageBoxHelper.ShowError(result.Message);
             }
+        }
+        await LoadBranchesAsync();
+    }
+    
+    public async Task DeleteAsync(long groupId)
+    {
+        await _branchService.DeleteAsync(groupId);
+    }
+
+    public async Task EditAsync(long branchId)
+    {
+        _editingBranchId = branchId;
+        var branch = await _branchService.GetByIdAsync(branchId);
+        BranchName = branch.Name;
+    }
+    
+    private async Task LoadBranchesAsync()
+    {
+        AllBranches.Clear();
+        var groups = await _branchService.GetAllAsync();
+        foreach (var g in groups)
+        {
+            AllBranches.Add(g);
         }
     }
 
--- SarGoldACC.WpfApp/Views/Branch.xaml.cs
+++ SarGoldACC.WpfApp/Views/Branch.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Input;
+using SarGoldACC.Core.DTOs.Branch;
 using SarGoldACC.Core.Services.Interfaces;
 using SarGoldACC.WpfApp.ViewModels;
 
@@ -44,5 +47,33 @@ public partial class Branch : Window
     {
         await _viewModel.SaveBranch();
         BranchNameBox.Text = "";
+    }
+
+    private void BranchDataGrid_Loaded(object sender, RoutedEventArgs e)
+    {
+        BranchDataGrid.DeleteActionShow = _viewModel.CanAccessBranchDelete;
+        BranchDataGrid.EditActionShow = _viewModel.CanAccessBranchEdit;
+        
+        BranchDataGrid.DeleteAction = async obj =>
+        {
+            if (obj is BranchDto branch)
+            {
+                await _viewModel.DeleteAsync(branch.Id);
+            }
+        };
+        
+        BranchDataGrid.EditAction = async obj =>
+        {
+            if (obj is BranchDto branch)
+            {
+                await _viewModel.EditAsync(branch.Id);
+            }
+        };
+        
+        BranchDataGrid.ColumnConfigKey = $"BranchGrid_{_authorizationService.GetCurrentUserIdAsString()}"; // یا هر شناسه خاصی که می‌خواهید
+        BranchDataGrid.SetColumns(
+            new DataGridTextColumn { Header = "شناسه", Binding = new Binding("Id"), Width = new DataGridLength(1, DataGridLengthUnitType.Star) },
+            new DataGridTextColumn { Header = "شعبه", Binding = new Binding("Name"), Width = new DataGridLength(5, DataGridLengthUnitType.Star) }
+        );
     }
 }

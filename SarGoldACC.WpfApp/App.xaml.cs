7e2ebc558da3ae522a7df9598e2d373544fb6616
moosavi <s.ha.mousavi.nazar@gmail.com>
2025-05-14 20:42:24 +0330

    حذف HandyControl

--- SarGoldACC.WpfApp/App.xaml.cs
+++ SarGoldACC.WpfApp/App.xaml.cs
@@ -28,12 +28,6 @@ namespace SarGoldACC.WpfApp
             
             base.OnStartup(e);
             
-            FrameworkElement.LanguageProperty.OverrideMetadata(
-                typeof(FrameworkElement),
-                new FrameworkPropertyMetadata(
-                    XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
-
-            
             Configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
